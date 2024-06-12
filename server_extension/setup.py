8dcd4596665683a67e39b3796382b2ab8a2d0f11
Ali Shobeiri <alishobeiri@ymail.com>
2024-06-12 13:13:20 -0600

    Publish and progress package

--- server_extension/setup.py
+++ server_extension/setup.py
@@ -14,7 +14,7 @@ long_description = long_description.replace('public/', 'thread/static/')
 
 setup(
     name="thread-dev",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
