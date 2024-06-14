1b2ec9f3617a595a52a66a9567788f39ffe66b5c
Bardia Khosravi <bardiakhosravi95@gmail.com>
2024-06-14 12:01:55 -0400

    added slack icon

--- src/assets/icons/svgs/index.tsx
+++ src/assets/icons/svgs/index.tsx
@@ -1359,6 +1359,15 @@ export const ServerIcon = createIcon({
 		></path>
 	),
 });
+export const SlackIcon = createIcon({
+	displayName: "SlackIcon",
+	viewBox: "0 0 640 512",
+	path: (
+		<>
+		<path d="M126.12,315.1A47.06,47.06,0,1,1,79.06,268h47.06Z"/><path d="M149.84,315.1a47.06,47.06,0,0,1,94.12,0V432.94a47.06,47.06,0,1,1-94.12,0Z"/><path d="M196.9,126.12A47.06,47.06,0,1,1,244,79.06v47.06Z"/><path d="M196.9,149.84a47.06,47.06,0,0,1,0,94.12H79.06a47.06,47.06,0,0,1,0-94.12Z"/><path d="M385.88,196.9A47.06,47.06,0,1,1,432.94,244H385.88Z"/><path d="M362.16,196.9a47.06,47.06,0,0,1-94.12,0V79.06a47.06,47.06,0,1,1,94.12,0Z"/><path d="M315.1,385.88A47.06,47.06,0,1,1,268,432.94V385.88Z"/><path d="M315.1,362.16a47.06,47.06,0,0,1,0-94.12H432.94a47.06,47.06,0,1,1,0,94.12Z"/>
+		</>
+	),
+});	
 
 export const InsertCellAboveIcon = createIcon({
 	displayName: "InsertCellAboveIcon",
@@ -1440,3 +1449,4 @@ export const InsertCellBelowIcon = createIcon({
 		</>
 	),
 });
+
