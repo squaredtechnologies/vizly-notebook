3f148c24929f9a026901eb1ca4f7700a4aecc861
Ali Shobeiri <alishobeiri@ymail.com>
2024-06-25 17:20:07 -0600

    Bump the package version

--- server_extension/setup.py
+++ server_extension/setup.py
@@ -14,7 +14,7 @@ long_description = long_description.replace('public/', 'thread/static/')
 
 setup(
     name="thread-dev",
-    version="0.1.17",
+    version="0.1.18",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
